6df1d7401285329e4af59e856875ae50fda2848b
efrec <quantumscrewdriver@gmail.com>
2024-08-03 01:50:26 -0400

    Remove resource values that are not being considered

--- luarules/gadgets/cmd_reserve_resources.lua
+++ luarules/gadgets/cmd_reserve_resources.lua
@@ -83,8 +83,9 @@ local setGadgetActivation -- Fix lexical scoping for cyclical reference, below.
 
 local function allowBuildStep(unitID, unitDefID, teamID, part)
     if not reservedUnits[teamID][unitID] then
-        local m, mstore, mpull, mgain, mlose, mshare, msent, mrcvd = spGetTeamResources(teamID, "metal")
-        local e, estore, epull, egain, elose, eshare, esent, ercvd = spGetTeamResources(teamID, "energy")
+    --  local m, mstore, mpull, mgain, mlose, mshare, msent, mrcvd
+        local m, mstore = spGetTeamResources(teamID, "metal")
+        local e, estore = spGetTeamResources(teamID, "energy")
 
         -- Always prioritize build progress during overflow.
         if msent > 0 and (esent > 0 or reservedEnergy[teamID] < 1) then
@@ -99,7 +100,7 @@ local function allowBuildStep(unitID, unitDefID, teamID, part)
         local mcost = unitDef.metalCost * part
         local ecost = unitDef.energyCost * part
 
-        if mstore < mreserve or m - mcost < mreserve or
+        if  mstore < mreserve or m - mcost < mreserve or
             estore < ereserve or e - ecost < ereserve
         then
             unitBuildMemo[teamID][unitID] = gameFrame + updateTime
