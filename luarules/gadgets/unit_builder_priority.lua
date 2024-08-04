5d2e0f4262ea92ff6eb40ee7a88af46326cc138a
efrec <quantumscrewdriver@gmail.com>
2024-08-04 01:30:31 -0400

    less bad init

--- luarules/gadgets/unit_builder_priority.lua
+++ luarules/gadgets/unit_builder_priority.lua
@@ -199,23 +199,14 @@ function gadget:Initialize()
 	restartReserveTracking()
 
     for _, unitID in ipairs(Spring.GetAllUnits()) do
-		-- Can't run UnitCreated directly anymore. Because of reserves.
-        -- gadget:UnitCreated(unitID, Spring.GetUnitDefID(unitID), spGetUnitTeam(unitID))
+		-- Can't just run UnitCreated anymore. Because of reserves.
+		local unitDefID, teamID = Spring.GetUnitDefID(unitID), spGetUnitTeam(unitID)
+        gadget:UnitCreated(unitID, unitDefID, teamID)
 
-		-- So initialize only the passive builders:
-		local unitDefID = Spring.GetUnitDefID(unitID)
-		local teamID = spGetUnitTeam(unitID)
-		if canPassive[unitDefID] or unitBuildSpeed[unitDefID] then
-			canBuild[teamID] = canBuild[teamID] or {}
-			canBuild[teamID][unitID] = true
-			realBuildSpeed[unitID] = unitBuildSpeed[unitDefID] or 0
-		end
-		if canPassive[unitDefID] then
-			spInsertUnitCmdDesc(unitID, cmdPassiveDesc)
-			if not passiveCons[teamID] then passiveCons[teamID] = {} end
-			passiveCons[teamID][unitID] = spGetUnitRulesParam(unitID,ruleName) == 1 or nil
-			currentBuildSpeed[unitID] = realBuildSpeed[unitID]
-			spSetUnitBuildSpeed(unitID, currentBuildSpeed[unitID]) -- to handle luarules reloads correctly
+		-- So also run UnitFinished on completed units.
+		local beingBuilt, progress = Spring.GetUnitIsBeingBuilt(unitID)
+		if beingBuilt or progress < 1 then
+			gadget:UnitFinished(unitID, unitDefID, teamID)
 		end
     end
 
