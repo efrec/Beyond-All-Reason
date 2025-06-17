6f4f7071d5a01ee6de162f62bf7984f96ab1dff3
efrec <quantumscrewdriver@gmail.com>
2025-06-17 15:17:50 -0400

    version alpha

--- luarules/gadgets/unit_reclaim_damage.lua
+++ luarules/gadgets/unit_reclaim_damage.lua
@@ -1,20 +1,20 @@
 local gadget = gadget ---@type Gadget
 
 function gadget:GetInfo()
-    return {
-        name      = 'Reclaim Degrades Features',
-        desc      = '',
-        author    = '',
-        version   = 'v0',
-        date      = '',
-        license   = 'GNU GPL, v2 or later',
-        layer     = 999999,
-        enabled   = true
-    }
+	return {
+		name    = 'Reclaim Degrades Features',
+		desc    = 'Reclaim damages wrecks; resurrect heals them. Only full-health wrecks gain resurrect progress.',
+		author  = 'efrec',
+		version = '0.1',
+		date    = '2025-06-17',
+		license = 'GNU GPL, v2 or later',
+		layer   = 0,
+		enabled = true,
+	}
 end
 
 if not gadgetHandler:IsSyncedCode() then
-    return false
+	return false
 end
 
 local math_clamp = math.clamp
@@ -28,15 +28,15 @@ local spSetFeatureHealth = Spring.SetFeatureHealth
 local healthMaxFractionalLimit = 10
 
 function gadget:AllowFeatureBuildStep(builderID, builderTeam, featureID, featureDefID, part)
-    local health, healthMax = spGetFeatureHealth(featureID)
-    local healthAfter = math_clamp(health / healthMax + part, 0, 1)
+	local health, healthMax = spGetFeatureHealth(featureID)
+	local healthAfter = math_clamp(health / healthMax + part, 0, 1)
 
-    if part < 0 and (healthAfter == 0 or (healthMax > healthMaxFractionalLimit and healthMax * healthAfter < 1)) then
-        spDestroyFeature(featureID)
-    elseif healthAfter < 1 then
-        spSetFeatureHealth(featureID, healthMax * healthAfter)
-        return part < 0
-    end
+	if part < 0 and (healthAfter == 0 or (healthMax > healthMaxFractionalLimit and healthMax * healthAfter < 1)) then
+		spDestroyFeature(featureID)
+	elseif healthAfter < 1 then
+		spSetFeatureHealth(featureID, healthMax * healthAfter)
+		return part < 0
+	end
 
-    return true
+	return true
 end
