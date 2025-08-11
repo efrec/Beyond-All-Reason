9de0004c4e0767fdd6637fdfe24fe6aaa0270018
efrec <quantumscrewdriver@gmail.com>
2025-08-11 17:25:17 -0400

    add ownership tug-o-war feature
    The naive ownership implementation earlier would allow a single funneler to refill and reclaim from a single wreck to fund an ally tax-free. While that remains the case here -- the amount of metal sent to an allied owner is not taxed in this implementation -- I've added this ownership transfer feature.
    
    Refilling a feature with metal just to reclaim it again will transfer ownership to the refiller over time. The result is that a unit's wreck can be used, really, only once. With extraordinary hustle, you could get multiple uses out of it, but I just do not see BAR players ever becoming so motivated.

--- luarules/gadgets/feature_resource_siphon_ownership.lua
+++ luarules/gadgets/feature_resource_siphon_ownership.lua
@@ -3,6 +3,11 @@ local gadget = gadget ---@type Gadget
 -- Part of a collection of changes to prevent resource sharing:
 local enabled = Spring.GetModOptions().resource_siphons == "ownership"
 
+-- The "ownership" method sends reclaim back to the original unit's owner.
+-- It does not, as of yet, reestablish as a new owner a player who refills
+-- the metal missing from a wreckage; so at present it is a very efficient
+-- means of sending metal directly to another player via their unit wreck.
+
 function gadget:GetInfo()
 	return {
 		name    = "Anti-Siphoning: Ownership",
@@ -22,7 +27,9 @@ end
 
 -- Global values
 
+local math_max = math.max
 local spGetFeatureResources = Spring.GetFeatureResources
+local spGetTeamList = Spring.GetTeamList
 local spAreTeamsAllied = Spring.AreTeamsAllied
 
 -- Initialize
@@ -68,6 +75,44 @@ local function sendToAlliedOwner(builderTeam, feature, metal, energy)
 	feature.energy = energy
 end
 
+local function getPower(metal, energy)
+	return metal + energy / 60 -- In effect, power == metal.
+end
+
+---Filling a wreck with metal gradually increases ownership of the wreck.
+---Reclaiming from the same wreck is, eventually, not considered sharing.
+---Players who own a wreckage are given higher priority toward ownership.
+local function updateOwnership(builderTeam, feature, metal, energy, featureID)
+	local metalDiff  = feature.metal - metal
+	local energyDiff = feature.energy - energy
+	local progress   = getPower(metalDiff, energyDiff) / feature.power
+
+	if feature.team == builderTeam then
+		for _, team in ipairs(spGetTeamList()) do
+			if team ~= builderTeam and feature[team] then
+				-- Ownership perk: Your progress resets other teams'.
+				feature[team] = math_max(0, feature[team] - progress)
+			end
+		end
+	else
+		local ownership = (feature[builderTeam] or 0) + progress
+
+		if ownership < 1 then
+			feature[builderTeam] = ownership
+		else
+			feature.team = builderTeam
+			Spring.TransferFeature(featureID, builderTeam)
+			progress = ownership - 1
+
+			for _, team in ipairs(spGetTeamList()) do
+				if feature[team] then
+					feature[team] = math_max(0, feature[team] - progress)
+				end
+			end
+		end
+	end
+end
+
 -- Engine callins
 
 function gadget:FeatureCreated(featureID, featureAllyTeam)
@@ -77,9 +122,10 @@ function gadget:FeatureCreated(featureID, featureAllyTeam)
 		if featureTeam and featureTeam >= 0 then
 			local metal, _, energy = spGetFeatureResources(featureID)
 			featureTracking[featureID] = {
-				team   = featureTeam,
 				metal  = metal,
 				energy = energy,
+				power  = getPower(metal, energy),
+				team   = featureTeam,
 			}
 		end
 	end
@@ -90,10 +136,15 @@ function gadget:FeatureDestroyed(featureID, featureAllyTeam)
 end
 
 function gadget:AllowFeatureBuildStep(builderID, builderTeam, featureID, featureDefID, part)
-	if part < 0 and featureTracking[featureID] then
+	if featureTracking[featureID] then
 		local feature = featureTracking[featureID]
 		local metal, _, energy = spGetFeatureResources(featureID)
-		sendToAlliedOwner(builderTeam, feature, metal, energy)
+
+		if part < 0 then
+			sendToAlliedOwner(builderTeam, feature, metal, energy)
+		elseif part > 0 then
+			updateOwnership(builderTeam, feature, metal, energy, featureID)
+		end
 	end
 	return true
 end
