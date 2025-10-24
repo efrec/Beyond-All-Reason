e733aa6270742f6b2b2884d5ab795dcceaf78f1d
efrec <quantumscrewdriver@gmail.com>
2025-10-24 11:13:54 -0400

    s/uDef/unitDef

--- unitbasedefs/techsplit_balance_defs.lua
+++ unitbasedefs/techsplit_balance_defs.lua
@@ -1,100 +1,100 @@
-local function techsplit_balanceTweaks(name, uDef)
+local function techsplit_balanceTweaks(name, unitDef)
 	if name == "corthud" then
-		uDef.speed = 54
-		uDef.weapondefs.arm_ham.range = 300
-		uDef.weapondefs.arm_ham.predictboost = 0.8
+		unitDef.speed = 54
+		unitDef.weapondefs.arm_ham.range = 300
+		unitDef.weapondefs.arm_ham.predictboost = 0.8
 	elseif name == "armwar" then
-		uDef.speed = 60
-		uDef.weapondefs.armwar_laser.range = 280
+		unitDef.speed = 60
+		unitDef.weapondefs.armwar_laser.range = 280
 	elseif name == "corstorm" then
-		uDef.speed = 45
-		uDef.weapondefs.cor_bot_rocket.range = 600
-		uDef.weapondefs.cor_bot_rocket.reloadtime = 4.8
-		uDef.weapondefs.cor_bot_rocket.damage.default = 198
-		uDef.health = 385
+		unitDef.speed = 45
+		unitDef.weapondefs.cor_bot_rocket.range = 600
+		unitDef.weapondefs.cor_bot_rocket.reloadtime = 4.8
+		unitDef.weapondefs.cor_bot_rocket.damage.default = 198
+		unitDef.health = 385
 	elseif name == "armrock" then
-		uDef.speed = 50
-		uDef.weapondefs.arm_bot_rocket.range = 575
-		uDef.weapondefs.arm_bot_rocket.reloadtime = 4.6
-		uDef.weapondefs.arm_bot_rocket.damage.default = 190
-		uDef.health = 390
+		unitDef.speed = 50
+		unitDef.weapondefs.arm_bot_rocket.range = 575
+		unitDef.weapondefs.arm_bot_rocket.reloadtime = 4.6
+		unitDef.weapondefs.arm_bot_rocket.damage.default = 190
+		unitDef.health = 390
 	elseif name == "armhlt" then
-		uDef.health = 4640
-		uDef.metalcost = 535
-		uDef.energycost = 5700
-		uDef.buildtime = 13700
-		uDef.weapondefs.arm_laserh1.range = 750
-		uDef.weapondefs.arm_laserh1.reloadtime = 2.9
-		uDef.weapondefs.arm_laserh1.damage = {
+		unitDef.health = 4640
+		unitDef.metalcost = 535
+		unitDef.energycost = 5700
+		unitDef.buildtime = 13700
+		unitDef.weapondefs.arm_laserh1.range = 750
+		unitDef.weapondefs.arm_laserh1.reloadtime = 2.9
+		unitDef.weapondefs.arm_laserh1.damage = {
 			commanders = 801,
 			default = 534,
 			vtol = 48,
 		}
 	elseif name == "corhlt" then
-		uDef.health = 4640
-		uDef.metalcost = 580
-		uDef.energycost = 5700
-		uDef.buildtime = 13800
-		uDef.weapondefs.cor_laserh1.range = 750
-		uDef.weapondefs.cor_laserh1.reloadtime = 1.8
-		uDef.weapondefs.cor_laserh1.damage = {
+		unitDef.health = 4640
+		unitDef.metalcost = 580
+		unitDef.energycost = 5700
+		unitDef.buildtime = 13800
+		unitDef.weapondefs.cor_laserh1.range = 750
+		unitDef.weapondefs.cor_laserh1.reloadtime = 1.8
+		unitDef.weapondefs.cor_laserh1.damage = {
 			commanders = 540,
 			default = 360,
 			vtol = 41,
 		}
 	elseif name == "armart" then
-		uDef.speed = 65
-		uDef.turnrate = 210
-		uDef.maxacc = 0.018
-		uDef.maxdec = 0.081
-		uDef.weapondefs.tawf113_weapon.accuracy = 150
-		uDef.weapondefs.tawf113_weapon.range = 830
-		uDef.weapondefs.tawf113_weapon.damage = {
+		unitDef.speed = 65
+		unitDef.turnrate = 210
+		unitDef.maxacc = 0.018
+		unitDef.maxdec = 0.081
+		unitDef.weapondefs.tawf113_weapon.accuracy = 150
+		unitDef.weapondefs.tawf113_weapon.range = 830
+		unitDef.weapondefs.tawf113_weapon.damage = {
 			default = 182,
 			subs = 61,
 			vtol = 20,
 		}
-		uDef.weapons[1].maxangledif = 30
+		unitDef.weapons[1].maxangledif = 30
 	elseif name == "corwolv" then
-		uDef.speed = 62
-		uDef.turnrate = 250
-		uDef.maxacc = 0.015
-		uDef.maxdec = 0.0675
-		uDef.weapondefs.corwolv_gun.accuracy = 150
-		uDef.weapondefs.corwolv_gun.range = 850
-		uDef.weapondefs.corwolv_gun.damage = {
+		unitDef.speed = 62
+		unitDef.turnrate = 250
+		unitDef.maxacc = 0.015
+		unitDef.maxdec = 0.0675
+		unitDef.weapondefs.corwolv_gun.accuracy = 150
+		unitDef.weapondefs.corwolv_gun.range = 850
+		unitDef.weapondefs.corwolv_gun.damage = {
 			default = 375,
 			subs = 95,
 			vtol = 38,
 		}
-		uDef.weapons[1].maxangledif = 30
+		unitDef.weapons[1].maxangledif = 30
 	elseif name == "armmart" then
-		uDef.metalcost = 400
-		uDef.energycost = 5500
-		uDef.buildtime = 7500
-		uDef.speed = 47
-		uDef.turnrate = 120
-		uDef.maxacc = 0.005
-		uDef.health = 750
-		uDef.weapondefs.arm_artillery.accuracy = 75
-		uDef.weapondefs.arm_artillery.areaofeffect = 60
-		uDef.weapondefs.arm_artillery.hightrajectory = 1
-		uDef.weapondefs.arm_artillery.range = 1140
-		uDef.weapondefs.arm_artillery.reloadtime = 3.05
-		uDef.weapondefs.arm_artillery.damage = {
+		unitDef.metalcost = 400
+		unitDef.energycost = 5500
+		unitDef.buildtime = 7500
+		unitDef.speed = 47
+		unitDef.turnrate = 120
+		unitDef.maxacc = 0.005
+		unitDef.health = 750
+		unitDef.weapondefs.arm_artillery.accuracy = 75
+		unitDef.weapondefs.arm_artillery.areaofeffect = 60
+		unitDef.weapondefs.arm_artillery.hightrajectory = 1
+		unitDef.weapondefs.arm_artillery.range = 1140
+		unitDef.weapondefs.arm_artillery.reloadtime = 3.05
+		unitDef.weapondefs.arm_artillery.damage = {
 			default = 488,
 			subs = 122,
 			vtol = 49,
 		}
-		uDef.weapons[1].maxangledif = 30
+		unitDef.weapons[1].maxangledif = 30
 	elseif name == "cormart" then
-		uDef.metalcost = 600
-		uDef.energycost = 6600
-		uDef.buildtime = 6500
-		uDef.speed = 45
-		uDef.turnrate = 100
-		uDef.maxacc = 0.005
-		uDef.weapondefs.cor_artillery = {
+		unitDef.metalcost = 600
+		unitDef.energycost = 6600
+		unitDef.buildtime = 6500
+		unitDef.speed = 45
+		unitDef.turnrate = 100
+		unitDef.maxacc = 0.005
+		unitDef.weapondefs.cor_artillery = {
 			accuracy = 75,
 			areaofeffect = 75,
 			avoidfeature = false,
@@ -123,151 +123,151 @@ local function techsplit_balanceTweaks(name, uDef)
 				vtol = 120,
 			},
 		}
-		uDef.weapons[1].maxangledif = 30
+		unitDef.weapons[1].maxangledif = 30
 	elseif name == "armfido" then
-		uDef.speed = 74
-		uDef.weapondefs.bfido.range = 500
-		uDef.weapondefs.bfido.weaponvelocity = 400
+		unitDef.speed = 74
+		unitDef.weapondefs.bfido.range = 500
+		unitDef.weapondefs.bfido.weaponvelocity = 400
 	elseif name == "cormort" then
-		uDef.health = 800
-		uDef.metalcost = 325
-		uDef.speed = 51
-		uDef.weapondefs.cor_mort.range = 650
+		unitDef.health = 800
+		unitDef.metalcost = 325
+		unitDef.speed = 51
+		unitDef.weapondefs.cor_mort.range = 650
 	elseif name == "corhrk" then
-		uDef.turnrate = 600
-		uDef.weapondefs.corhrk_rocket.range = 900
-		uDef.weapondefs.corhrk_rocket.weaponvelocity = 600
-		uDef.weapondefs.corhrk_rocket.flighttime = 22
-		uDef.weapondefs.corhrk_rocket.reloadtime = 8
-		uDef.weapondefs.corhrk_rocket.turnrate = 30000
-		uDef.weapondefs.corhrk_rocket.weapontimer = 4
-		uDef.weapons[1].maxangledif = 60
-		uDef.weapons[1].maindir = "0 0 1"
+		unitDef.turnrate = 600
+		unitDef.weapondefs.corhrk_rocket.range = 900
+		unitDef.weapondefs.corhrk_rocket.weaponvelocity = 600
+		unitDef.weapondefs.corhrk_rocket.flighttime = 22
+		unitDef.weapondefs.corhrk_rocket.reloadtime = 8
+		unitDef.weapondefs.corhrk_rocket.turnrate = 30000
+		unitDef.weapondefs.corhrk_rocket.weapontimer = 4
+		unitDef.weapons[1].maxangledif = 60
+		unitDef.weapons[1].maindir = "0 0 1"
 	elseif name == "armsptk" then
-		uDef.metalcost = 500
-		uDef.speed = 43
-		uDef.health = 450
-		uDef.turnrate = 600
-		uDef.weapondefs.adv_rocket.range = 775
-		uDef.weapondefs.adv_rocket.trajectoryheight = 1
-		uDef.weapondefs.adv_rocket.customparams.overrange_distance = 800
-		uDef.weapondefs.adv_rocket.weapontimer = 8
-		uDef.weapondefs.adv_rocket.flighttime = 4
-		uDef.weapons[1].maxangledif = 45
-		uDef.weapons[1].maindir = "0 0 1"
+		unitDef.metalcost = 500
+		unitDef.speed = 43
+		unitDef.health = 450
+		unitDef.turnrate = 600
+		unitDef.weapondefs.adv_rocket.range = 775
+		unitDef.weapondefs.adv_rocket.trajectoryheight = 1
+		unitDef.weapondefs.adv_rocket.customparams.overrange_distance = 800
+		unitDef.weapondefs.adv_rocket.weapontimer = 8
+		unitDef.weapondefs.adv_rocket.flighttime = 4
+		unitDef.weapons[1].maxangledif = 45
+		unitDef.weapons[1].maindir = "0 0 1"
 	elseif name == "corshiva" then
-		uDef.speed = 55
-		uDef.weapondefs.shiva_gun.range = 475
-		uDef.weapondefs.shiva_gun.areaofeffect = 180
-		uDef.weapondefs.shiva_gun.weaponvelocity = 372
-		uDef.weapondefs.shiva_rocket.areaofeffect = 96
-		uDef.weapondefs.shiva_rocket.range = 900
-		uDef.weapondefs.shiva_rocket.reloadtime = 14
-		uDef.weapondefs.shiva_rocket.damage.default = 1500
+		unitDef.speed = 55
+		unitDef.weapondefs.shiva_gun.range = 475
+		unitDef.weapondefs.shiva_gun.areaofeffect = 180
+		unitDef.weapondefs.shiva_gun.weaponvelocity = 372
+		unitDef.weapondefs.shiva_rocket.areaofeffect = 96
+		unitDef.weapondefs.shiva_rocket.range = 900
+		unitDef.weapondefs.shiva_rocket.reloadtime = 14
+		unitDef.weapondefs.shiva_rocket.damage.default = 1500
 	elseif name == "armmar" then
-		uDef.health = 3920
-		uDef.weapondefs.armmech_cannon.areaofeffect = 48
-		uDef.weapondefs.armmech_cannon.range = 275
-		uDef.weapondefs.armmech_cannon.reloadtime = 1.25
-		uDef.weapondefs.armmech_cannon.damage = {
+		unitDef.health = 3920
+		unitDef.weapondefs.armmech_cannon.areaofeffect = 48
+		unitDef.weapondefs.armmech_cannon.range = 275
+		unitDef.weapondefs.armmech_cannon.reloadtime = 1.25
+		unitDef.weapondefs.armmech_cannon.damage = {
 			default = 525,
 			vtol = 134,
 		}
 	elseif name == "corban" then
-		uDef.speed = 69
-		uDef.turnrate = 500
-		uDef.weapondefs.banisher.areaofeffect = 180
-		uDef.weapondefs.banisher.range = 400
+		unitDef.speed = 69
+		unitDef.turnrate = 500
+		unitDef.weapondefs.banisher.areaofeffect = 180
+		unitDef.weapondefs.banisher.range = 400
 	elseif name == "armcroc" then
-		uDef.turnrate = 270
-		uDef.weapondefs.arm_triton.reloadtime = 1.5
-		uDef.weapondefs.arm_triton.damage = {
+		unitDef.turnrate = 270
+		unitDef.weapondefs.arm_triton.reloadtime = 1.5
+		unitDef.weapondefs.arm_triton.damage = {
 			default = 250,
 			subs = 111,
 			vtol = 44
 		}
-		uDef.weapons[2] = {
+		unitDef.weapons[2] = {
 			def = "",
 		}
 	elseif name == "correap" then
-		uDef.speed = 76
-		uDef.turnrate = 250
-		uDef.weapondefs.cor_reap.areaofeffect = 92
-		uDef.weapondefs.cor_reap.damage = {
+		unitDef.speed = 76
+		unitDef.turnrate = 250
+		unitDef.weapondefs.cor_reap.areaofeffect = 92
+		unitDef.weapondefs.cor_reap.damage = {
 			default = 150,
 			vtol = 48,
 		}
-		uDef.weapondefs.cor_reap.range = 305
+		unitDef.weapondefs.cor_reap.range = 305
 	elseif name == "armbull" then
-		uDef.health = 6000
-		uDef.metalcost = 1100
-		uDef.weapondefs.arm_bull.range = 400
-		uDef.weapondefs.arm_bull.damage = {
+		unitDef.health = 6000
+		unitDef.metalcost = 1100
+		unitDef.weapondefs.arm_bull.range = 400
+		unitDef.weapondefs.arm_bull.damage = {
 			default = 600,
 			subs = 222,
 			vtol = 67
 		}
-		uDef.weapondefs.arm_bull.reloadtime = 2
-		uDef.weapondefs.arm_bull.areaofeffect = 96
+		unitDef.weapondefs.arm_bull.reloadtime = 2
+		unitDef.weapondefs.arm_bull.areaofeffect = 96
 	elseif name == "corsumo" then
-		uDef.weapondefs.corsumo_weapon.range = 750
-		uDef.weapondefs.corsumo_weapon.damage = {
+		unitDef.weapondefs.corsumo_weapon.range = 750
+		unitDef.weapondefs.corsumo_weapon.damage = {
 			default = 700,
 			vtol = 165,
 		}
-		uDef.weapondefs.corsumo_weapon.reloadtime = 1
+		unitDef.weapondefs.corsumo_weapon.reloadtime = 1
 	elseif name == "corgol" then
-		uDef.speed = 37
-		uDef.weapondefs.cor_gol.damage = {
+		unitDef.speed = 37
+		unitDef.weapondefs.cor_gol.damage = {
 			default = 1600,
 			subs = 356,
 			vtol = 98,
 		}
-		uDef.weapondefs.cor_gol.reloadtime = 4
-		uDef.weapondefs.cor_gol.range = 700
+		unitDef.weapondefs.cor_gol.reloadtime = 4
+		unitDef.weapondefs.cor_gol.range = 700
 	elseif name == "armguard" then
-		uDef.health = 6000
-		uDef.metalcost = 800
-		uDef.energycost = 8000
-		uDef.buildtime = 16000
-		uDef.weapondefs.plasma.areaofeffect = 150
-		uDef.weapondefs.plasma.range = 1000
-		uDef.weapondefs.plasma.reloadtime = 2.3
-		uDef.weapondefs.plasma.weaponvelocity = 550
-		uDef.weapondefs.plasma.damage = {
+		unitDef.health = 6000
+		unitDef.metalcost = 800
+		unitDef.energycost = 8000
+		unitDef.buildtime = 16000
+		unitDef.weapondefs.plasma.areaofeffect = 150
+		unitDef.weapondefs.plasma.range = 1000
+		unitDef.weapondefs.plasma.reloadtime = 2.3
+		unitDef.weapondefs.plasma.weaponvelocity = 550
+		unitDef.weapondefs.plasma.damage = {
 			default = 140,
 			subs = 70,
 			vtol = 42,
 		}
-		uDef.weapondefs.plasma_high.areaofeffect = 150
-		uDef.weapondefs.plasma_high.range = 1000
-		uDef.weapondefs.plasma_high.reloadtime = 2.3
-		uDef.weapondefs.plasma_high.weaponvelocity = 700
-		uDef.weapondefs.plasma_high.damage = {
+		unitDef.weapondefs.plasma_high.areaofeffect = 150
+		unitDef.weapondefs.plasma_high.range = 1000
+		unitDef.weapondefs.plasma_high.reloadtime = 2.3
+		unitDef.weapondefs.plasma_high.weaponvelocity = 700
+		unitDef.weapondefs.plasma_high.damage = {
 			default = 140,
 			subs = 70,
 			vtol = 42,
 		}
 	elseif name == "corpun" then
-		uDef.health = 6400
-		uDef.metalcost = 870
-		uDef.energycost = 8700
-		uDef.buildtime = 16400
-		uDef.weapondefs.plasma.areaofeffect = 180
-		uDef.weapondefs.plasma.range = 1020
-		uDef.weapondefs.plasma.reloadtime = 2.3
-		uDef.weapondefs.plasma.weaponvelocity = 550
-		uDef.weapondefs.plasma.damage = {
+		unitDef.health = 6400
+		unitDef.metalcost = 870
+		unitDef.energycost = 8700
+		unitDef.buildtime = 16400
+		unitDef.weapondefs.plasma.areaofeffect = 180
+		unitDef.weapondefs.plasma.range = 1020
+		unitDef.weapondefs.plasma.reloadtime = 2.3
+		unitDef.weapondefs.plasma.weaponvelocity = 550
+		unitDef.weapondefs.plasma.damage = {
 			default = 163,
 			lboats = 163,
 			subs = 21,
 			vtol = 22,
 		}
-		uDef.weapondefs.plasma_high.areaofeffect = 180
-		uDef.weapondefs.plasma_high.range = 1020
-		uDef.weapondefs.plasma_high.reloadtime = 2.3
-		uDef.weapondefs.plasma_high.weaponvelocity = 700
-		uDef.weapondefs.plasma_high.damage = {
+		unitDef.weapondefs.plasma_high.areaofeffect = 180
+		unitDef.weapondefs.plasma_high.range = 1020
+		unitDef.weapondefs.plasma_high.reloadtime = 2.3
+		unitDef.weapondefs.plasma_high.weaponvelocity = 700
+		unitDef.weapondefs.plasma_high.damage = {
 			default = 163,
 			lboats = 163,
 			subs = 21,
@@ -275,7 +275,7 @@ local function techsplit_balanceTweaks(name, uDef)
 		}
 	end
 
-	return uDef
+	return unitDef
 end
 
 return {
