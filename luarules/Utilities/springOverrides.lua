42afddf4746a5f8410e7fdd271eff4fc46075e1b
efrec <quantumscrewdriver@gmail.com>
2025-11-26 15:34:49 -0500

    code that is good and is not bad

--- luarules/Utilities/springOverrides.lua
+++ luarules/Utilities/springOverrides.lua
@@ -7,6 +7,8 @@ Spring.Base = base
 if Spring.TraceScreenRay then
 	local sp_TraceScreenRay = Spring.TraceScreenRay
 
+	---@diagnostic disable return-type-mismatch -- FIXME: docs are wrong
+
 	---Get information about a ray traced from screen to world position.
 	--
 	-- This method is an override of the engine-provided TraceScreenRay,
@@ -21,28 +23,27 @@ if Spring.TraceScreenRay then
 	---@return ("unit"|"feature"|"ground"|"sky")? description of traced object or position
 	---@return (integer|xyz)? result unitID or featureID (integer), or position triple (xyz)
 	local function traceScreenRay(screenX, screenY, onlyCoords, useMinimap, includeSky, ignoreWater, heightOffset)
-		local peek = not useMinimap and (not onlyCoords or type(onlyCoords) ~= "boolean") -- arg3 can be heightOffset
-
-		if peek then
-			Script.LuaUI.RestoreSelectionVolume()
+		-- note: arg3 actually might be the heightOffset, which is a "number" type
+		if useMinimap or onlyCoords == true then
+			return sp_TraceScreenRay(
+				screenX,
+				screenY,
+				onlyCoords, -- ignores units (but maybe is heightOffset)
+				useMinimap, -- ignores unit volumes (queries by midpoint)
+				includeSky,
+				ignoreWater,
+				heightOffset
+			)
 		end
 
-		local description, result = sp_TraceScreenRay(
-			screenX,
-			screenY,
-			onlyCoords, -- ignores units
-			useMinimap, -- ignores unit volumes (queries by midpoint)
-			includeSky,
-			ignoreWater,
-			heightOffset
-		)
-
-		if peek then
-			Script.LuaUI.RemoveSelectionVolume()
-		end
-
-		return description, result ---@diagnostic disable-line return-type-mismatch -- FIXME: docs are wrong
+		-- Peek any hidden selection volumes so we get a true ray trace.
+		Script.LuaUI.RestoreSelectionVolume()
+		local description, result = sp_TraceScreenRay(screenX, screenY, onlyCoords, useMinimap, includeSky, ignoreWater, heightOffset)
+		Script.LuaUI.RemoveSelectionVolume()
+		return description, result
 	end
+
+	---@diagnostic enable return-type-mismatch -- FIXME: docs are wrong
 
 	base.TraceScreenRay = sp_TraceScreenRay
 	Spring.TraceScreenRay = traceScreenRay
