9b65be74166b6d15e9b83f5fffbd491518fae5c5
efrec <quantumscrewdriver@gmail.com>
2026-07-31 13:34:29 -0400

    unused

--- spec/mission_api/triggers/unit_discovered_spec.lua
+++ spec/mission_api/triggers/unit_discovered_spec.lua
@@ -8,9 +8,6 @@ GG['MissionAPI'].Modules.ParameterTypes = VFS.Include('luarules/mission_api/para
 
 _G.UnitDefs = { [1] = { name = 'armpw' }, [2] = { name = 'corfast' } }
 
-_G.Game = _G.Game or {}
-_G.Game.gameSpeed = 30
-
 local unitDiscovered = VFS.Include('luarules/mission_api/triggers/unit_discovered.lua')
 local onEnteredLos = unitDiscovered.callins.UnitEnteredLos
 local onEnteredRadar = unitDiscovered.callins.UnitEnteredRadar
